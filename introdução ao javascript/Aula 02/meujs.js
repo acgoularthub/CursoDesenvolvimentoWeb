d7575a0e231975ec71d5e875e15a625ab87b147e
anderson Kali <acgoulartmail@gmail.com>
2020-06-08 14:48:36 +0000

    Aulas 1 e 2 de introcução ao javascript adicionadas

---
+++ introdução ao javascript/Aula 02/meujs.js
@@ -0,0 +1,32 @@
+//aula 02, inclusão de variáveis
+
+//string
+var texto = "Aula 02 de javascript";
+
+//floa
+var numFracionado = 2.5;
+
+//int
+var numInteiro = 2;
+
+//bool
+var teste = true;
+
+/*
+
+toda variável não precisa ser declarada explicitamente,
+porém, uma vez inserido um valor, o tipo desse valor não pode
+ser trocado.
+*/
+
+alert(texto + ", " + numInteiro + ", " + numFracionado);
+document.write(texto);
+
+//tipos de comandos de saída:
+
+/*
+console.log(""); --> mostra no console do navegador
+document.write(""); --> escreve na pagina, na parte onde foi chamado
+alert(""); --> cria uma popup com o conteúdo indicado.
+
+*/
---
+++ introdução ao javascript/Aula 02/outrojs.js
@@ -0,0 +1,5 @@
+var texto2 = "tentativa de testes 2";
+
+var num = 12345;
+
+var num2 = 12.34;
