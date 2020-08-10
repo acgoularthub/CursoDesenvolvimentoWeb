51a15d5e7c099f51809657c08ba2e20634763fe8
Anderson Coelho Goulart <acgoulartmail@gmail.com>
2020-08-10 14:06:51 +0000

    Finalizado jogo da velha, corrigido bug do click sobrescrito.

--- projeto final jquery - jogo da velha/jogo.js
+++ projeto final jquery - jogo da velha/jogo.js
@@ -1,6 +1,23 @@
 var rodada = 1;
 var matriz = Array(3);
 
+matriz['a'] = Array(3);
+matriz['b'] = Array(3);
+matriz['c'] = Array(3);
+
+
+matriz['a'][1] = 0;
+matriz['a'][2] = 0;
+matriz['a'][3] = 0;
+
+matriz['b'][1] = 0;
+matriz['b'][2] = 0;
+matriz['b'][3] = 0;
+
+matriz['c'][1] = 0;
+matriz['c'][2] = 0;
+matriz['c'][3] = 0;
+
 $(document).ready(function(){
 
 		$('#btn_iniciar').click(function(){
@@ -30,7 +47,8 @@ $(document).ready(function(){
 
 		$('.jogada').click(function(){
 			var id_campo_clicado = this.id;
-			alert(id_campo_clicado);
+			jogada(id_campo_clicado);
+			$('#' + id_campo_clicado).off();
 		});
 
 		function jogada(id){
@@ -38,6 +56,97 @@ $(document).ready(function(){
 			var icone = '';
 			var ponto = 0;
 
+			if((rodada %2) == 1){
+				icone = 'url("imagens/marcacao_1.png")';
+				ponto = -1;
+			}else{
+				icone = 'url("imagens/marcacao_2.png")';
+				ponto = 1;
+			}
+
+			rodada++;
+
+			$('#'+ id).css('background-image',icone);
+
+			var linha_coluna = id.split('-');
+
+			matriz[linha_coluna[0]][linha_coluna[1]] = ponto;
+			verifica_campo();
+
+
+
 		}
 
+
+
+		function verifica_campo(){
+
+			//verificação horizontal
+			
+			var pontos = 0;
+
+			for (var i = 1; i <= 3; i++){
+				pontos += matriz['a'][i]
+			}
+			
+			verifica_campeao(pontos);
+			pontos = 0;
+
+			for (var i = 1; i <= 3; i++){
+				pontos += matriz['b'][i]
+			}
+			verifica_campeao(pontos);
+			pontos = 0;
+
+			for (var i = 1; i <= 3; i++){
+				pontos += matriz['c'][i]
+			}
+			verifica_campeao(pontos);
+			
+
+			//verificação vertical
+
+			for(var j = 1; j <= 3; j++){
+				pontos = 0;
+				pontos += matriz['a'][j];
+				pontos += matriz['b'][j];
+				pontos += matriz['c'][j];
+
+				verifica_campeao(pontos);
+
+			}
+		
+
+			//verificação diagonal
+
+			pontos = 0;
+			pontos = matriz['a'][1] + matriz['b'][2] + matriz['c'][3];
+			verifica_campeao(pontos);
+
+			pontos = 0;
+			pontos = matriz['a'][3] + matriz['b'][2] + matriz['c'][1];
+			verifica_campeao(pontos);
+			
+		}
+
+
+
+		//verifica quem venceu
+
+		function verifica_campeao(pontos){
+			if (pontos == -3){
+				var jogador1 = $('#player1').val();
+				alert(jogador1 + ' venceu!');
+				$('.jogada').off();
+
+			}else if(pontos == 3){
+				var jogador2 = $('#player2').val();
+				alert(jogador2 + ' venceu!');
+				$('.jogada').off();
+				
+					}	
+			
+
+		}
+			
 	});
